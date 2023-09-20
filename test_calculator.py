07da7bb68865c2358bfc74b95279521a0fe29b7c
Robert Portelli <github@robertportelli.com>
2023-09-20 18:35:43 -0400

    add remainder test

--- test_calculator.py
+++ test_calculator.py
@@ -10,11 +10,14 @@ class TestCalculator:
     def test_addition(self):
         assert 4 == calculator.add(2, 2)
 
-    def test_subtractio(self):
+    def test_subtraction(self):
         assert 2 == calculator.subtract(4, 2)
 
     def test_multiplication(self):
         assert 100 == calculator.multiplication(10, 10)
 
-    def test_division(sefl):
+    def test_division(self):
         assert 42 == calculator.division(84, 2)
+
+    def test_remainder(self):
+        assert 0 == calculator.remainder(4, 2)
